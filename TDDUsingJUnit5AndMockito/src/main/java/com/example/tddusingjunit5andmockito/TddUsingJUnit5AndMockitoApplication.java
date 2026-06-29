0806cccfab9a8582c6d269fc451b8a2729eadc2b
Suhani Sharma <145221387+suhanijourneyongithub@users.noreply.github.com>
2026-06-29 12:00:00 +0530

    JUnit_Basic Testing Exercise 1: Setting Up JUnit

---
+++ TDDUsingJUnit5AndMockito/src/main/java/com/example/tddusingjunit5andmockito/TddUsingJUnit5AndMockitoApplication.java
@@ -0,0 +1,13 @@
+package com.example.tddusingjunit5andmockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddUsingJUnit5AndMockitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TddUsingJUnit5AndMockitoApplication.class, args);
+    }
+
+}
---
+++ TDDUsingJUnit5AndMockito/src/test/java/com/example/tddusingjunit5andmockito/TddUsingJUnit5AndMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tddusingjunit5andmockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddUsingJUnit5AndMockitoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
